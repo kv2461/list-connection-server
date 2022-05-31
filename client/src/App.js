7124bd3b55df6b057395a86944564d7fada5b59a
kv2461 <kevinarnoldtv@gmail.com>
2022-05-30 22:53:25 -0700

    added PostCreation component, for now routed from App, moved state into app, and just a clone of Form component. Thinking of altering Form component so when there is no current user? not in localStorage but same current user being used for editing button, that it functions differently

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container } from '@mui/material';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import NavBar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import PostCreation from './components/PostCreation/PostCreation';
 
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
+
+  const [currentId,setCurrentId] = useState(0);
+
+
 
   return (
       <BrowserRouter>
@@ -15,8 +20,10 @@ const App = () => {
           <NavBar />
           <Routes>
             <Route path='/' element={<Navigate to='/posts/'/>}/>
-            <Route path='/posts' element={<Home />}/>
+            <Route path='/posts' element={<Home currentId={currentId} setCurrentId={setCurrentId}/>}/>
             <Route path='/auth' element={(user===null)?<Auth />:<Navigate to='/posts/'/>} />
+            <Route path='/createPost' element={(user!==null)?<PostCreation currentId={currentId} setCurrentId={setCurrentId}/>:<Navigate to='/posts'/>} />
+         
           </Routes>
         </Container>
       </BrowserRouter>
---
+++ client/src/components/PostCreation/PostCreation.js
@@ -0,0 +1,68 @@
+import React, {useState,useEffect} from 'react';
+import {TextField, Button, Typography} from '@mui/material';
+import FileBase from 'react-file-base64';
+import {StyledForm, StyledButton, StyledPaper, StyledFileInput } from './styles';
+import {useDispatch, useSelector} from 'react-redux';
+import {CreatePost, UpdatePost} from '../../actions/posts';
+
+const PostCreation = ({currentId,setCurrentId}) => {
+    const [postData,setPostData] = useState({
+      title:'', description:'', tags:'', selectedFile:'',
+    });
+    const post = useSelector((state)=>currentId?state.postsSlice.find((p)=>p._id===currentId):null);
+
+    const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    useEffect(()=>{
+      if(post) setPostData(post);
+    },[post])
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if(currentId===0) {
+        dispatch(CreatePost({...postData, username: user?.result?.username}));
+      } else {
+      dispatch(UpdatePost(currentId, {...postData, username: user?.result?.username}));
+      }
+      clear();
+    }
+
+    if(!user?.result?.name) {
+      return(
+        <StyledPaper>
+          <Typography variant='h6' align='center'>
+            Need to sign in
+          </Typography>
+        </StyledPaper>
+      )
+    }
+
+    const clear = () => {
+      setCurrentId(0);
+      setPostData({title:'',description:'',tags:'',selectedFile:''});
+    }
+    
+  return (
+    <StyledPaper>
+      <StyledForm autoComplete='off' noValidate onSubmit={handleSubmit}>
+        <Typography variant='h6'>{currentId?'Edit':'Make'} a List</Typography>
+        <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e) =>setPostData({...postData, title:e.target.value})}/>
+        <TextField name='description' variant='outlined' label='Description' fullWidth value={postData.description} onChange={(e) =>setPostData({...postData, description:e.target.value})}/>
+        <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e) =>setPostData({...postData, tags:e.target.value.split(',')})}/>
+        <StyledFileInput>
+          <FileBase 
+            type='file'
+            multiple={false}
+            onDone={({base64})=> setPostData({...postData,selectedFile:base64})}
+          />
+        </StyledFileInput>
+        <StyledButton sx={{bgcolor:'primary.main'}}variant='container' size='large' type='submit' fullWidth>Submit</StyledButton>
+        <Button sx={{bgcolor:'secondary.main'}}variant='container' size='small' onClick={clear} fullWidth>Clear</Button>
+      </StyledForm>
+    </StyledPaper>
+  )
+}
+
+export default PostCreation
