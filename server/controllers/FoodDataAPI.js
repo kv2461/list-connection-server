505992d458f7101b2645ab926c536ea6a1bdcfdf
kv2461 <kevinarnoldtv@gmail.com>
2022-06-08 01:27:24 -0700

    added a new food route api, not happy about current route naming in FoodDataRoutes

--- server/controllers/FoodDataAPI.js
+++ server/controllers/FoodDataAPI.js
@@ -14,9 +14,48 @@ export const getFoodData = async (req,res) => {
     try {
 
         axios.get(api_url)
-        .then((response)=> {
+        .then((response) => {
             res.status(200).json(response.data);
         })
+
+    } catch (error) {
+        res.status(404).json({message:error.message})
+    }
+}
+
+export const getSpoonacularData = async (req,res) => {
+    const { query } = req.params;
+    const api_key = process.env.SPOONACULAR_APIKEY;
+
+    const api_url = `https://api.spoonacular.com/food/ingredients/search?apiKey=${api_key}&query=${query}`
+
+    try {
+        axios.get(api_url)
+        .then((response) => {
+            res.status(200).json(response.data);
+        })
+
+    } catch (error) {
+        res.status(404).json({message:error.message})
+    }
+}
+
+export const getSpoonacularSpecificData = async (req,res) => {
+    const { query } = req.params;
+    const api_key = process.env.SPOONACULAR_APIKEY;
+
+    const api_url = `https://api.spoonacular.com/food/ingredients/search?apiKey=${api_key}&query=${query}`
+
+
+    try {
+        axios.get(api_url)
+        .then((response) => {
+            const {id} = response.data.results[0];
+            axios.get(`https://api.spoonacular.com/food/ingredients/${id}/information?apiKey=${api_key}&amount=1`)
+            .then((response) => {
+                res.status(200).json(response.data);
+            });
+        });
 
     } catch (error) {
         res.status(404).json({message:error.message})
--- server/routes/FoodDataRoutes.js
+++ server/routes/FoodDataRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
-import { getFoodData } from '../controllers/FoodDataAPI.js';
+import { getFoodData, getSpoonacularData, getSpoonacularSpecificData } from '../controllers/FoodDataAPI.js';
 
 const router = express.Router();
 
 router.get('/search/:query',getFoodData);
+
+router.get('/lookup/:query',getSpoonacularData);
+
+router.get('/find/:query',getSpoonacularSpecificData);
 
 export default router;
