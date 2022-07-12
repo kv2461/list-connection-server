48f42ffa6f13a07d5439effe5d24b74c5292e32c
kv2461 <kevinarnoldtv@gmail.com>
2022-07-12 15:27:16 -0700

    changed server to heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL : 'http://localhost:4001' });
+const API = axios.create({ baseURL : 'https://list-connection.herokuapp.com' });
 
 API.interceptors.request.use((req)=> {
     if(localStorage.getItem('profile')) {
